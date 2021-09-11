f172ebabbd657739ce5e251e8ab8256864a161a3
embunazzahra <dhauembunazzahra@gmail.com>
2021-09-11 10:50:49 +0700

    adding some method

--- jmart/DhauEmbunAzzahraJmartPK/Jmart.java
+++ jmart/DhauEmbunAzzahraJmartPK/Jmart.java
@@ -3,27 +3,35 @@ package DhauEmbunAzzahraJmartPK;
 
 public class Jmart
 {
-
-    private int x;
-
-    /**
-     * Constructor for objects of class Jmart
-     */
-    public Jmart()
-    {
-        // initialise instance variables
-        x = 0;
+    public static int getPromo(){
+        return 0;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    public static String getCustomer(){
+        return "oop";
+}
+    public static float  getDiscountPercentage(int before, int after){
+        float hasil;
+   
+        if (before<after){
+            return 0.0f;
+        }
+        else{
+            hasil = ((float)(after-before)/(float)before)*100;
+            return hasil*(-1);
+        }
+    }
+    
+    public static int getDiscountedPrice(int price, float discountPercentage){
+        if (discountPercentage>=100.0f){
+            return 0;
+        }
+        else{
+            return (int)((100.0f-discountPercentage)*price/100);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(getDiscountPercentage(1000, 0));
+        System.out.println(getDiscountedPrice(1000, 10.0f));
     }
 }
