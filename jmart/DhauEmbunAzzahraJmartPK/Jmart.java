70ead6eba5912c7db2551e6beba7aa0602082cc3
embunazzahra <dhauembunazzahra@gmail.com>
2021-09-11 10:11:01 +0700

    adding Jmart Class

---
+++ jmart/DhauEmbunAzzahraJmartPK/Jmart.java
@@ -0,0 +1,29 @@
+package DhauEmbunAzzahraJmartPK;
+
+
+public class Jmart
+{
+
+    private int x;
+
+    /**
+     * Constructor for objects of class Jmart
+     */
+    public Jmart()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
