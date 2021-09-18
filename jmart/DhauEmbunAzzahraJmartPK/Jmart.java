778c099cfa4aac95424c6423405b94c18f8966ea
embunazzahra <dhauembunazzahra@gmail.com>
2021-09-18 10:51:56 +0700

    CS-Modul 2-lab pk

--- jmart/DhauEmbunAzzahraJmartPK/Jmart.java
+++ jmart/DhauEmbunAzzahraJmartPK/Jmart.java
@@ -3,68 +3,12 @@ package DhauEmbunAzzahraJmartPK;
 
 public class Jmart
 {
-    public static int getPromo()
-    {
-        return 0;
-    }
-    
-    public static String getCustomer()
-    {
-        return "oop";
-    }
-    public static float  getDiscountPercentage(int before, int after)
-    {
-        float hasil;
-   
-        if (before<=after || before == 0)
-        {
-            return 0.0f;
-        }
-        else
-        {
-            hasil = ((float)(before-after)/(float)before)*100;
-            return hasil;
-        }
-    }
-    
-    public static int getDiscountedPrice(int price, float discountPercentage)
-    {
-        if (discountPercentage>=100.0f)
-        {
-            return 0;
-        }
-        else
-        {
-            return (int)((100.0f-discountPercentage)*price/100.0f);
-        }
-    }
-    
-    public static int getOriginalPrice(int discountedPrice, float discountPercentage)
-    {
-        if (discountPercentage>=100.0f)
-        {
-            return 0;
-        }
-        else
-        {
-            return (int) discountedPrice*100/(int)(100.0f-discountPercentage);
-        }
-    }
-    public static float getCommissionMultiplier()
-    {
-        return 0.05f;
-    }
-    public static int getAdjustedPrice(int price)
-    {
-        return (int) ((getCommissionMultiplier()+1)*price);
-    }
-    public static int getAdminFee(int price)
-    {
-        return (int) (price*getCommissionMultiplier());
+    public static Product create(){
+        return new Product("Embun", 8, true, new PriceTag(70.0), ProductCategory.BOOK);
     }
     public static void main(String[] args) 
     {
-
+        
                 
     }
 }
