226fb50a070a4740ea730dd941fea15af242fd6b
embunazzahra <dhauembunazzahra@gmail.com>
2021-09-24 14:33:10 +0700

    revisi mod 2

--- jmart/DhauEmbunAzzahraJmartPK/Jmart.java
+++ jmart/DhauEmbunAzzahraJmartPK/Jmart.java
@@ -3,9 +3,18 @@ package DhauEmbunAzzahraJmartPK;
 
 public class Jmart
 {
-    public static Product create(){
+    public static Product createProduct(){
         return new Product("Embun", 8, true, new PriceTag(70.0), ProductCategory.BOOK);
     }
+    
+    public static Coupon createCoupon(){
+        return null;
+    }
+    
+    public static ShipmentDuration createShipmentDuration(){
+        return null;
+    }
+    
     public static void main(String[] args) 
     {
         
--- jmart/DhauEmbunAzzahraJmartPK/ShipmentDuration.java
+++ jmart/DhauEmbunAzzahraJmartPK/ShipmentDuration.java
@@ -5,17 +5,30 @@ package DhauEmbunAzzahraJmartPK;
 public class ShipmentDuration
 {
     public static final ShipmentDuration INSTANT = new ShipmentDuration(1<<0);
-    public static final ShipmentDuration SAME_DAY = new ShipmentDuration(1<<2);
-    public static final ShipmentDuration NEXT_DAY = new ShipmentDuration(1<<3);
-    public static final ShipmentDuration REGULER = new ShipmentDuration(1<<4);
-    public static final ShipmentDuration KARGO = new ShipmentDuration(1<<5);
-    private int bit;
+    public static final ShipmentDuration SAME_DAY = new ShipmentDuration(1<<1);
+    public static final ShipmentDuration NEXT_DAY = new ShipmentDuration(1<<2);
+    public static final ShipmentDuration REGULER = new ShipmentDuration(1<<3);
+    public static final ShipmentDuration KARGO = new ShipmentDuration(1<<4);
+    private final int bit;
     
     private ShipmentDuration(int bit){
         this.bit = bit;
     }
     
     public ShipmentDuration(ShipmentDuration... args){
-        
+        int save = 0;
+        for (ShipmentDuration x:args){
+            save = (save | x.bit);
+        }
+        this.bit = save;
+    }
+    
+    public boolean isDuration(ShipmentDuration reference){
+        if((this.bit & reference.bit) != 0){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
