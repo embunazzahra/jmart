4b86a22e4eab8e219556699d15f5ce57b28b0412
embunazzahra <dhauembunazzahra@gmail.com>
2021-09-27 21:36:13 +0700

    revisi postest mod 3

--- jmart/DhauEmbunAzzahraJmartPK/Shipment.java
+++ jmart/DhauEmbunAzzahraJmartPK/Shipment.java
@@ -17,24 +17,24 @@ public class Shipment implements FileParser{
         throw new UnsupportedOperationException("Not supported yet."); 
     }
     public static class Duration{
-        public static final Duration INSTANT = new Duration(1<<0);
-        public static final Duration SAME_DAY = new Duration(1<<1);
-        public static final Duration NEXT_DAY = new Duration(1<<2);
-        public static final Duration REGULER = new Duration(1<<3);
-        public static final Duration KARGO = new Duration(1<<4);
-        private final byte bit;
+        public static final Duration INSTANT = new Duration((byte)(1<<0));
+        public static final Duration SAME_DAY = new Duration((byte)(1<<1));
+        public static final Duration NEXT_DAY = new Duration((byte)(1<<2));
+        public static final Duration REGULER = new Duration((byte)(1<<3));
+        public static final Duration KARGO = new Duration((byte)(1<<4));
+        public final byte bit;
     
-        private Duration(int bit){
-            this.bit = (byte)bit;
+        private Duration(byte bit){
+            this.bit = bit;
         }
     }//end duration
     
     public static class MultiDuration{
-        public int bit;
+        public byte bit;
         public MultiDuration(Duration... args){
-            int save = 0;
+            byte save = 0;
             for (Duration x:args){
-                save = (save | x.bit);
+                save = (byte) (save | x.bit);
             }
             this.bit = save;
         }
