76bdf2f50d2fc6f123e3ff2584dcd79ac9d8a4ac
embunazzahra <dhauembunazzahra@gmail.com>
2021-11-04 15:11:32 +0700

    tp-pk-modul 6

--- src/DhauEmbunAzzahraJmartPK/Jmart.java
+++ src/DhauEmbunAzzahraJmartPK/Jmart.java
@@ -1,13 +1,34 @@
 package DhauEmbunAzzahraJmartPK;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import com.google.gson.*;
 
 
 public class Jmart
 {
-    
+    class Country{
+        public String name;
+        public int population;
+        public List<String> listOfStates;
+    }
     
     public static void main(String[] args) 
     {
-        
+        String filepath = "C://Proyek Jmart/Jmart/src/lib/city.json";
+        Gson gson = new Gson();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filepath));
+            Country input = gson.fromJson(br, Country.class);
+            System.out.println("name: "+input.name);
+            System.out.println("population: "+input.population);
+            System.out.println("states:");
+            input.listOfStates.forEach(state-> System.out.println(state));
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
                 
     }
 }
